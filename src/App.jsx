a1c3088387ddebc5773691643d4877c29e82ac18
Chiamaka Prosper Nkwazema <149527450+prosperpaul@users.noreply.github.com>
2024-07-11 11:56:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+    </>
+  );
+};
+
+export default App;
